976f2881b1c70596aa1aa2ebf762285c0edaa65d
rektdie <rektdie@gmail.com>
2023-06-12 16:41:25 +0200

    Now can add books (not visually).

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const newButton = document.querySelector("#new-book");
+const dialog = document.querySelector("dialog");
+const cancelButton = document.querySelector("#cancel");
+const form = document.querySelector("form");
 
 newButton.addEventListener("click", () => {
-    addBookToLibrary();
+    dialog.setAttribute("open", "");
+});
+
+cancelButton.addEventListener("click", event => {
+    event.preventDefault();
+    dialog.removeAttribute("open");
+});
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = formData.get("pages");
+    const read = formData.get("read");
+
+    myLibrary.push(new Book(title, author, pages, read));
+    form.reset();
+    //listBooks();
 });
 
 let myLibrary = [];
@@ -17,6 +40,8 @@ function Book(title, author, pages, read){
     };
 }
 
-function addBookToLibrary() {
-    
+function listBooks(){
+    for (book in myLibrary){
+        // Create a card for each book and append them to 'books' element
+    }
 }
