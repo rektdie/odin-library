89295adc0362f8eae9218c800d29eec27146f741
rektdie <rektdie@gmail.com>
2023-06-22 12:31:33 +0200

    Refactored the book constructor to class

--- index.js
+++ index.js
@@ -31,11 +31,13 @@ form.addEventListener("submit", e => {
 
 let myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book{
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function listBooks(){
